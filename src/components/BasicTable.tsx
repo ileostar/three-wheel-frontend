4dc21f1c6bfc128a09de8301e90038533b2015cd
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-10-23 16:14:25 +0800

    core: Temporary file

--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -1,12 +1,99 @@
-import { Pagination } from 'antd'
+import { Table } from 'antd'
 
+import type { ColumnsType, TableProps } from 'antd/es/table'
+
+interface DataType {
+  stuName: string
+  stuNum: number
+  sex: string
+  email: string
+  grade: number
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: '姓名',
+    dataIndex: 'stuName',
+  },
+  {
+    title: '性别',
+    dataIndex: 'sex',
+  },
+  {
+    title: '学号',
+    dataIndex: 'stuNum',
+    sorter: {
+      compare: (a, b) => a.stuNum - b.stuNum,
+      multiple: 3,
+    },
+  },
+  {
+    title: '年级',
+    dataIndex: 'grade',
+    sorter: {
+      compare: (a, b) => a.grade - b.grade,
+      multiple: 3,
+    },
+  },
+  {
+    title: '邮箱',
+    dataIndex: 'email',
+  },
+]
+
+const data: DataType[] = [
+  {
+    stuName: '张三',
+    stuNum: 123,
+    sex: '男',
+    email: '123@qq.com',
+    grade: 2022,
+  },
+  {
+    stuName: '张三',
+    stuNum: 123,
+    sex: '男',
+    email: '123@qq.com',
+    grade: 2022,
+  },
+  {
+    stuName: '张三',
+    stuNum: 123,
+    sex: '男',
+    email: '123@qq.com',
+    grade: 2022,
+  },
+  {
+    stuName: '张三',
+    stuNum: 123,
+    sex: '男',
+    email: '123@qq.com',
+    grade: 2022,
+  },
+  {
+    stuName: '张三',
+    stuNum: 123,
+    sex: '男',
+    email: '123@qq.com',
+    grade: 2022,
+  },
+  {
+    stuName: '张三',
+    stuNum: 123,
+    sex: '男',
+    email: '123@qq.com',
+    grade: 2022,
+  },
+]
+
+const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+  // eslint-disable-next-line no-console
+  console.log('params', pagination, filters, sorter, extra)
+}
 export default function Footer() {
   return (
-    <div className="flex justify-center flex-between h-full p-5 px-10 flex-col">
-      <div className="flex-1">
-        111
-      </div>
-      <Pagination simple defaultCurrent={2} total={50} />
+    <div className="flex justify-center flex-between h-full w-full p-5 px-10 flex-col">
+        <Table className="h-full" size="large" columns={columns} dataSource={data} onChange={onChange} />
     </div>
 
   )
